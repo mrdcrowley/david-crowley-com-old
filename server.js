974ec8dc304df158e06c992339db2b4df7b26875
David Crowley <david@slantback.net>
2013-04-23 21:17:06 -0700

    fixed tumblr not updating

--- server.js
+++ server.js
@@ -2,17 +2,17 @@
  * Module dependencies
  */
 var express = require('express')
-  , stylus = require('stylus')
-  , nib = require('nib')
-  , Tumblr = require('tumblrwks')
+	, stylus = require('stylus')
+	, nib = require('nib')
+	, Tumblr = require('tumblrwks')
 
 var app = express();
 
 var tumblr = new Tumblr(
-  {
-	consumerKey: 'wbv0QjwCd2yUEODaaUOXR8Ni7P8phsWUNzYZqnK4lNUVNMpA36'
-  }, "slantback.tumblr.com"
-);
+	{
+		consumerKey: 'wbv0QjwCd2yUEODaaUOXR8Ni7P8phsWUNzYZqnK4lNUVNMpA36'
+	}, "slantback.tumblr.com"
+)
 
 function compile(str, path) {
   return stylus(str)
@@ -34,21 +34,24 @@ app.use(stylus.middleware(
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-	res.render('index', { 
-  		title : 'Home',
+	res.render('index', {
+		title : 'Home',
 		cssID : 'pageHome'
-  	})
+	})
+	getTumblrPosts()
 })
 
 app.get('/work', function (req, res) {
-	res.render('work', { 
+	res.render('work', {
 		title : 'Work',
 		cssID : 'pageWork'
 	})
 })
 
-tumblr.get('/posts', {hostname: 'slantback.tumblr.com'}, function(json){
-	postsTumblr = json.posts
-});
+function getTumblrPosts(){
+	tumblr.get('/posts', {hostname: 'slantback.tumblr.com'}, function(json){
+		postsTumblr = json.posts
+	})
+}
 
 app.listen(3000)
