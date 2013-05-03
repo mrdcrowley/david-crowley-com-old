326d1d587e850e84f8483b902cd37a00e0559418
David Crowley <david@slantback.net>
2013-05-02 19:59:04 -0700

    modal window for portfolio images

--- public/js/david-crowley-com.js
+++ public/js/david-crowley-com.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
+
+	// lazy loading for images
 	$("img.thumb, ul.posts li img").lazyload({
 		effect : "fadeIn"
+	});
+	
+	// modal window for images in portfolio
+	$('#pageWork a[href$=".jpg"], #pageWork a[href$=".png"], #pageWork a[href$=".gif"]').boxer({
+    	margin: 20
 	});
 });
