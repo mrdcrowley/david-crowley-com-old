bd2bb72788a674facef200b5164250451045ad28
David Crowley <david@slantback.net>
2013-05-02 23:07:22 -0700

    styling tweaks

--- public/js/david-crowley-com.js
+++ public/js/david-crowley-com.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
 	$("img.thumb, ul.posts li img").lazyload({
 		effect : "fadeIn"
 	});
-	
+
 	// modal window for images in portfolio
 	$('#pageWork a[href$=".jpg"], #pageWork a[href$=".png"], #pageWork a[href$=".gif"]').boxer({
-    	margin: 20
+		margin: 20
 	});
 });
