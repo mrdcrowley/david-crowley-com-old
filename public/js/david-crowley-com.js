b7082d3ca49ee622e4b273fb9da4a2a495f9a778
David Crowley <david@slantback.net>
2013-05-17 01:43:41 -0700

    timeline working

--- public/js/david-crowley-com.js
+++ public/js/david-crowley-com.js
@@ -9,4 +9,19 @@ $(document).ready(function() {
 	$('#pageWork a[href$=".jpg"], #pageWork a[href$=".png"], #pageWork a[href$=".gif"]').boxer({
 		margin: 20
 	});
+
+	// timeline
+	createStoryJS({
+		width:              '100%',
+		height:             '600',
+		embed_id:           'timeline-embed',
+		start_at_end:       false,
+		start_at_slide:     '1',
+		start_zoom_adjust:  '3',
+		hash_bookmark:      true,
+		font:               'Bevan-PotanoSans',
+		debug:              true,
+		lang:               'en',
+		maptype:            'watercolor'
+	});
 });
